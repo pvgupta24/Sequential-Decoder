4e5b6fb8002af0880203d78383514e3f98fe46bd
Praveen Gupta <pvgupta24@gmail.com>
2018-02-17 23:25:24 +0530

    Update decode.m

--- decode.m
+++ decode.m
@@ -1,5 +1,7 @@
-function ans = decode(input, g, curr_state)
-%This function decodes the given input
+function code = decode(input, g, curr_state, error_count, threshhold)
+%============ This function decodes the given input ==================%
+curr_pair = input(1:3)
+threshhod
 
-ans = [];
+code = [];
 end
