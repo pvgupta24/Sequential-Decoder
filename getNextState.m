495e759056acff8d39401f9e3d9e8195e9e59ff8
Shashank P <1997shashankp@gmail.com>
2018-02-18 22:07:29 +0530

    getNextState function commit 2

--- getNextState.m
+++ getNextState.m
@@ -14,8 +14,8 @@ end
 %This function calculates the 2 bit convolutional output during state transition
 function [g1_bit, g2_bit] = conv_2bit(input, cur_state, g1, g2)  
     g_len = length(g1);    % length of generator 
-    g1_bit = input;        
-    g2_bit = input;        
+    g1_bit = input * g1(1);        
+    g2_bit = input * g2(1);        
     for i = 2:g_len       
         if(g1(i) == 1)
             g1_bit = bitxor(g1_bit, cur_state(i-1));
