dc598659e33ec78af482e9db3f56b5a321cba5a7
Shashank P <1997shashankp@gmail.com>
2018-02-19 10:04:52 +0530

    Added Decode function

--- main.m
+++ main.m
@@ -1,11 +1,12 @@
 %==========  Given generating functions and threshhold ==========%
-g{1} = [1 1 0 1 1 1 0 0 1];
-g{2} = [1 1 1 0 1 1 0 0 1];
+g1 = [1 1 0 1 1 1 0 0 1];
+g2 = [1 1 1 0 1 1 0 0 1];
 threshhold = 5;
 %========== 10 Zeroes for starting state (in Trellis) ==========%
-start_state = [0 0 0 0 0 0 0 0 0 0]
-
+start_state = [0 0 0 0 0 0 0 0 0 0];
+error_count =0;
+m = 11;
 %========== 8 Bit Input to Decode ==========%
 input = [1 0 1 0 1 0 1 0];
 
-decode(input, g, start_state)
+decode(input, g1, g2, start_state, error_count, threshhold, m)
